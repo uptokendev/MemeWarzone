357231fa4afd90bb02554ccd4c825486e80ac552
uptokendev <uptokendev@gmail.com>
2026-08-27 15:01:39 +0200

    feat(robinhood): boot V3 continuity indexer

--- realtime-indexer/src/wtrPreload.ts
+++ realtime-indexer/src/wtrPreload.ts
@@ -4,6 +4,7 @@ import { registerCanonicalCandleRoutes } from "./canonicalCandleApi.js";
 import { startGraduationReconcilerLoop } from "./graduationReconciler.js";
 import { registerMarketContinuityRoutes } from "./marketApi.js";
 import { startTopazPoolIndexerLoop } from "./topazPoolIndexer.js";
+import { startRobinhoodV3PoolIndexerLoop } from "./robinhoodV3PoolIndexer.js";
 
 const WTR_ROUTES_SYMBOL = Symbol.for("memewarzone.wtrMarketRoutesRegistered");
 const originalListen = express.application.listen as unknown as (
@@ -19,6 +20,7 @@ express.application.listen = function wtrPatchedListen(this: any, ...args: any[]
     console.log("[wtr] market continuity routes registered", {
       ENABLE_UNIFIED_MARKET_API: ENV.ENABLE_UNIFIED_MARKET_API,
       ENABLE_TOPAZ_POOL_INDEXER: ENV.ENABLE_TOPAZ_POOL_INDEXER,
+      ENABLE_ROBINHOOD_V3_POOL_INDEXER: String(process.env.ENABLE_ROBINHOOD_V3_POOL_INDEXER || "0") === "1",
       ENABLE_GRADUATION_HANDOFF_RECONCILER: ENV.ENABLE_GRADUATION_HANDOFF_RECONCILER,
     });
   }
@@ -28,8 +30,10 @@ express.application.listen = function wtrPatchedListen(this: any, ...args: any[]
 console.log("[wtr] preload boot flags", {
   ENABLE_UNIFIED_MARKET_API: ENV.ENABLE_UNIFIED_MARKET_API,
   ENABLE_TOPAZ_POOL_INDEXER: ENV.ENABLE_TOPAZ_POOL_INDEXER,
+  ENABLE_ROBINHOOD_V3_POOL_INDEXER: String(process.env.ENABLE_ROBINHOOD_V3_POOL_INDEXER || "0") === "1",
   ENABLE_GRADUATION_HANDOFF_RECONCILER: ENV.ENABLE_GRADUATION_HANDOFF_RECONCILER,
 });
 
 startGraduationReconcilerLoop();
 startTopazPoolIndexerLoop();
+startRobinhoodV3PoolIndexerLoop();
